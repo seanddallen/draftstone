0d8b5d46ff129654a75cb97f439a73f7d0b5eb53
Sean Tayler <seanjtayler@gmail.com>
2018-08-28 19:14:25 -0700

    updated JS

--- main.js
+++ main.js
@@ -15,7 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function renderPick(array) {
   //append 3 images. Add 3 Ids (img0-img2)
+  img0 = document.getElementById('img0');
+  img1 = document.getElementById('img1');
+  img2 = document.getElementById('img2');
+
+  img0.innerHTML = `<img id="img0" class="responsive" src="${array[0].img}" alt="">`
+  img1.innerHTML = `<img id="img1" class="responsive" src="${array[1].img}" alt="">`
+  img2.innerHTML = `<img id="img2" class="responsive" src="${array[2].img}" alt="">`
 }
+
+// const sampleDeck3 = [{"cardId":"EX1_162","dbfId":"985","name":"Dire Wolf Alpha","cardSet":"Classic","type":"Minion","faction":"Neutral","rarity":"Common","cost":2,"collectible":true,"playerClass":"Neutral","img":"http://media.services.zam.com/v1/media/byName/hs/cards/enus/EX1_162.png"},{"cardId":"EX1_044","dbfId":"791","name":"QuestingAdventurer","cardSet":"Classic","type":"Minion","faction":"Alliance","rarity":"Rare","cost":3,"attack":2,"health":2,"text":"Whenever you play a card, gain +1/+1.","flavor":"\"Does anyone have some extra Boar Pelts?\"","artist":"Attila Adorjany","collectible":true,"playerClass":"Neutral","img":"http://media.services.zam.com/v1/media/byName/hs/cards/enus/EX1_044.png"},{"cardId":"OG_161","dbfId":"38545","name":"Corrupted Seer","cardSet":"Whispers of the Old Gods","type":"Minion","rarity":"Rare","cost":6,"collectible":true,"race":"Murloc","playerClass":"Neutral","img":"http://media.services.zam.com/v1/media/byName/hs/cards/enus/OG_161.png"}];
+//
+// renderPick(sampleDeck3);
 
 ///////////////////////// RENDER DECK /////////////////////////
 
