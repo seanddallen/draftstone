c3cbbd689a86d0d8a38c2aab801afd869208cf86
Sean Tayler <seanjtayler@gmail.com>
2018-08-30 13:26:47 -0700

    merged files

--- scripts/draft.js
+++ scripts/draft.js
@@ -1,13 +1,227 @@
 // import { encode, decode, FormatType } from "deckstrings";
+//
+// const masterPool = JSON.parse(localStorage.getItem("masterPool"));
+// const heroes = JSON.parse(localStorage.getItem("heroes"));
+// const formatRules = JSON.parse(localStorage.getItem("formatRules"));
+//
+// ///////////////////////// GLOBAL VARIABLES /////////////////////////
+//
+// // const masterPool = []; // All collectible cards
+// // const heroes = []; // The nine original heroes to represent classes
+// let heroCard = {};
+// let selectedClass = ''; // User selected class
+// let filteredPool = masterPool; // Pool of only Neutral and class cards matching selected class
+// const deck = []; // Drafted deck
+// let pickOptions = []; // The three cards in any one pick
+// const cardDisplay = document.getElementById('card-display');
+// //const setArray = ["Basic", "Classic", "Hall of Fame", "Naxxramas", "Goblins vs Gnomes", "Blackrock Mountain", "The Grand Tournament", "The League of Explorers", "Whispers of the Old Gods", "One Night in Karazhan", "Mean Streets of Gadgetzan", "Journey to Un'Goro", "Knights of the Frozen Throne", "Kobolds & Catacombs", "The Witchwood", "The Boomsday Project"]; // Stores sets chosen by user
+//
+// ///////////////////////// START DRAFT /////////////////////////
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//
+//  if (formatRules.setArray[0] !== "All") {
+//    filterPoolBySet(formatRules.setArray);
+//
+//  }
+//   classPick();
+// });
+//
+// function filterPoolBySet(setArray) {
+//   filteredPool =  filteredPool.filter(card => setArray.includes(card.cardSet));
+// }
+//
+// ///////////////////////// PICK CLASS /////////////////////////
+//
+// function renderPick(array) {
+//   img0 = document.getElementById('img0');
+//   img1 = document.getElementById('img1');
+//   img2 = document.getElementById('img2');
+//
+//   //append 3 images
+//   img0.innerHTML = `<img id="img0" class="responsive" src="${array[0].img}" alt="">`
+//   img1.innerHTML = `<img id="img1" class="responsive" src="${array[1].img}" alt="">`
+//   img2.innerHTML = `<img id="img2" class="responsive" src="${array[2].img}" alt="">`
+// }
+//
+// // User chooses class
+// function classPick() {
+//   pickOptions = [];
+//   const selectedIndices = []; // To log indices and avoid duplicates
+//   let currentSelection = 0;
+//
+//  // Build array of options for pick
+//   for (let i = 0; i < 3; i++) {
+//     do {
+//       currentSelection = Math.floor(Math.random() * 9);
+//     } while (selectedIndices.includes(currentSelection)); // Verify index has not already been selected
+//     selectedIndices.push(currentSelection);
+//
+//     // Add randomly selected hero to pick
+//     pickOptions.push(heroes[currentSelection]);
+//   }
+//   hide = document.getElementById('hide');
+//   hide.classList.remove('hidden');
+//
+//   renderPick(pickOptions);
+//   cardDisplay.addEventListener('click', classPickHandler);
+// }
+//
+//
+// function classPickHandler(e) {
+//   // Ensure target is a card image
+//   if (e.target && e.target.id.includes("img")) {
+//
+//     // Remove event listener so it only triggers once
+//     cardDisplay.removeEventListener('click', classPickHandler);
+//
+//     // Use id of target to determine which option was selected
+//     const position = +e.target.id.slice(-1);
+//
+//     heroCard = pickOptions[position];
+//     selectedClass = heroCard.playerClass;
+//
+//     // Filter the master pool down to exclude all class cards that are not of chosen class
+//     filteredPool = filteredPool.filter(card => card.playerClass === "Neutral" || card.playerClass === selectedClass);
+//
+//     renderClassName()
+//     // Move flow of program to card picking stage
+//     cardPick();
+//   }
+// }
+//
+// ///////////////////////// PICK DECK /////////////////////////
+//
+// // User chooses a card
+// function cardPick() {
+//   pickOptions = [];
+//
+//   const selectedIndices = []; // To log indices and avoid duplicates
+//   let currentSelection = 0;
+//
+//   // Build array of options for pick
+//   for (let i = 0; i < 3; i++) {
+//     do {
+//       currentSelection = Math.floor(Math.random() * filteredPool.length);
+//     } while (selectedIndices.includes(currentSelection) || maxxedAlready(filteredPool[currentSelection])); // Verify index has not already been selected and that two copies of card are not already in deck
+//     selectedIndices.push(currentSelection);
+//
+//     // Add randomly selected card to pick
+//     pickOptions.push(filteredPool[currentSelection]);
+//   }
+//
+//   renderPick(pickOptions);
+//   cardDisplay.addEventListener('click', cardPickHandler);
+// }
+//
+// // Function that determines if given card is already a 2-of in the deck
+// function maxxedAlready(proposedCard) {
+//   let numInDeck = 0;
+//   for (const card of deck) {
+//     if (card.cardId === proposedCard.cardId) {
+//       numInDeck++;
+//       if (numInDeck === 2 || (card.rarity === "Legendary" && numInDeck === 1)) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+//
+//
+// function cardPickHandler(e) {
+//   // Ensure target is a card image
+//   if (e.target && e.target.id.includes("img")) {
+//     // Remove event listener to avoid multiple triggers
+//     cardDisplay.removeEventListener('click', cardPickHandler);
+//
+//     // Use id of target to determine which option was selected
+//     const position = +e.target.id.slice(-1);
+//
+//     // Add choosen card to deck
+//     deck.push(pickOptions[position]);
+//
+//     updateProgress()
+//     sortDeck();
+//     renderDeck(deck);
+//
+//     // Restart card pick process until 30 picks are made
+//     if (deck.length < 30) {
+//       cardPick();
+//     }
+//
+//     // If 30 picks have been made, advance the flow of the program
+//     if (deck.length === 30) {
+//       deckComplete();
+//     }
+//   }
+//
+//   // If user clicks red x indicating they don't own the card
+//     if (e.target && e.target.id.includes("xbtn")) {
+//       // Removed unowned card and replace with new random card
+//       let newOption = filteredPool[Math.floor(Math.random() * filteredPool.length)];
+//
+//       // TODO: Ensure that the newOption is not the same as one of the curent options
+//       while (maxxedAlready(newOption)) {
+//         newOption = filteredPool[Math.floor(Math.random() * filteredPool.length)];
+//       }
+//       pickOptions.splice(+e.target.id.slice(-1) - 1, 1, newOption);
+//       renderPick(pickOptions);
+//     }
+// }
+//
+//
+// // Sort first by cost, and then alphabetically by card name
+// function sortDeck() {
+//   deck.sort((cardA, cardB) => {
+//     if (cardA.cost < cardB.cost) {
+//       return -1;
+//     }
+//     if (cardA.cost > cardB.cost) {
+//       return 1;
+//     }
+//     return cardA.name.localeCompare(cardB.name);
+//   });
+// }
+//
+// function renderDeck(array) {
+//   cardName = document.getElementById('card-name');
+//   cardCost = document.getElementById('card-cost');
+//   cardName.innerHTML = '';
+//   cardCost.innerHTML = '';
+//
+//   array.forEach(function(card) {
+//     counter = 0;
+//     // if (counter < 10) {
+//
+//   // }
+//     cardName.innerHTML += `
+//       <div id="cards00" class="flex name-style">
+//         ${card.name}
+//       </div>
+//     `
+//   })
+//
+//   array.forEach(function(card) {
+//     cardCost.innerHTML += `
+//       <div class="flex cost-style">
+//         ${card.cost}
+//       </div>
+//     `
+//   })
+//   counter++
+// }
 
+
+
+
+
+// import { encode, decode, FormatType } from "deckstrings";
+///////////////////////// GLOBAL VARIABLES /////////////////////////
 const masterPool = JSON.parse(localStorage.getItem("masterPool"));
 const heroes = JSON.parse(localStorage.getItem("heroes"));
-const formatRules = JSON.parse(localStorage.getItem("formatRules"));
-
-///////////////////////// GLOBAL VARIABLES /////////////////////////
-
-// const masterPool = []; // All collectible cards
-// const heroes = []; // The nine original heroes to represent classes
+const custom = JSON.parse(localStorage.getItem("custom"));
+const customRules = JSON.parse(localStorage.getItem("customRules"));
 let heroCard = {};
 let selectedClass = ''; // User selected class
 let filteredPool = masterPool; // Pool of only Neutral and class cards matching selected class
@@ -15,105 +229,169 @@ const deck = []; // Drafted deck
 let pickOptions = []; // The three cards in any one pick
 const cardDisplay = document.getElementById('card-display');
 //const setArray = ["Basic", "Classic", "Hall of Fame", "Naxxramas", "Goblins vs Gnomes", "Blackrock Mountain", "The Grand Tournament", "The League of Explorers", "Whispers of the Old Gods", "One Night in Karazhan", "Mean Streets of Gadgetzan", "Journey to Un'Goro", "Knights of the Frozen Throne", "Kobolds & Catacombs", "The Witchwood", "The Boomsday Project"]; // Stores sets chosen by user
-
-///////////////////////// START DRAFT /////////////////////////
-
+const setArray = JSON.parse(localStorage.getItem("chosenSets"));
+let pools = {
+  'neutral': {
+    'minion': {
+      'Legendary': [],
+      'Epic': [],
+      'Rare': [],
+      'Common': []
+    },
+    'spell': {
+      'Legendary': [],
+      'Epic': [],
+      'Rare': [],
+      'Common': []
+    }
+  },
+  'class': {
+    'minion': {
+      'Legendary': [],
+      'Epic': [],
+      'Rare': [],
+      'Common': []
+    },
+    'spell': {
+      'Legendary': [],
+      'Epic': [],
+      'Rare': [],
+      'Common': []
+    }
+  }
+};
+const randomClass = [];
+const randomType = [];
+const randomRarity = [];
+////////////////////////
+///////////////////////// GET DATA /////////////////////////
 document.addEventListener('DOMContentLoaded', () => {
-
- if (formatRules.setArray[0] !== "All") {
-   filterPoolBySet(formatRules.setArray);
-
- }
+  if (setArray[0] !== "All") {
+    filterPoolBySet(setArray);
+  }
   classPick();
 });
-
 function filterPoolBySet(setArray) {
   filteredPool =  filteredPool.filter(card => setArray.includes(card.cardSet));
 }
-
+function setupCustom() {
+    for (let i = 0, numCards = filteredPool.length; i < numCards; i++) {
+      const currentCard = filteredPool[0];
+      const cardClass = currentCard.playerClass === "Neutral" ? 'neutral' : 'class';
+      const cardType = currentCard.type === "Minion" ? 'minion' : 'spell';
+      const cardRarity = currentCard.rarity === "Free" ? 'Common' : currentCard.rarity;
+      pools[cardClass][cardType][cardRarity].push(filteredPool.shift());
+    }
+    for (let i = 0; i < customRules.classCount; i++) {
+      randomClass.push("class");
+    }
+    while (randomClass.length < 30) {
+      randomClass.push("neutral");
+    }
+    for (let i = 0; i < customRules.spellCount; i++) {
+      randomType.push("spell");
+    }
+    while (randomType.length < 30) {
+      randomType.push("minion");
+    }
+    for (let i = 0; i < customRules.legendaryCount; i++) {
+      randomRarity.push("Legendary");
+    }
+    for (let i = 0; i < customRules.epicCount; i++) {
+      randomRarity.push("Epic");
+    }
+    for (let i = 0; i < customRules.rareCount; i++) {
+      randomRarity.push("Rare");
+    }
+    while (randomRarity.length < 30) {
+      randomRarity.push("Common");
+    }
+}
 ///////////////////////// PICK CLASS /////////////////////////
-
 function renderPick(array) {
   img0 = document.getElementById('img0');
   img1 = document.getElementById('img1');
   img2 = document.getElementById('img2');
-
   //append 3 images
-  img0.innerHTML = `<img id="img0" class="responsive" src="${array[0].img}" alt="">`
-  img1.innerHTML = `<img id="img1" class="responsive" src="${array[1].img}" alt="">`
-  img2.innerHTML = `<img id="img2" class="responsive" src="${array[2].img}" alt="">`
+  img0.innerHTML = `<img id="img0" class="responsive" src="${array[0].img}" alt="">`;
+  img1.innerHTML = `<img id="img1" class="responsive" src="${array[1].img}" alt="">`;
+  img2.innerHTML = `<img id="img2" class="responsive" src="${array[2].img}" alt="">`;
 }
-
 // User chooses class
 function classPick() {
   pickOptions = [];
   const selectedIndices = []; // To log indices and avoid duplicates
   let currentSelection = 0;
-
  // Build array of options for pick
   for (let i = 0; i < 3; i++) {
     do {
       currentSelection = Math.floor(Math.random() * 9);
     } while (selectedIndices.includes(currentSelection)); // Verify index has not already been selected
     selectedIndices.push(currentSelection);
-
     // Add randomly selected hero to pick
     pickOptions.push(heroes[currentSelection]);
   }
   hide = document.getElementById('hide');
   hide.classList.remove('hidden');
-
   renderPick(pickOptions);
   cardDisplay.addEventListener('click', classPickHandler);
 }
-
-
 function classPickHandler(e) {
   // Ensure target is a card image
   if (e.target && e.target.id.includes("img")) {
-
     // Remove event listener so it only triggers once
     cardDisplay.removeEventListener('click', classPickHandler);
-
     // Use id of target to determine which option was selected
     const position = +e.target.id.slice(-1);
-
-    heroCard = pickOptions[position];
-    selectedClass = heroCard.playerClass;
-
+    selectedClass = pickOptions[position].playerClass;
     // Filter the master pool down to exclude all class cards that are not of chosen class
     filteredPool = filteredPool.filter(card => card.playerClass === "Neutral" || card.playerClass === selectedClass);
-
-    renderClassName()
+    if (custom) {
+      setupCustom();
+    }
+    renderClassName();
     // Move flow of program to card picking stage
     cardPick();
   }
 }
-
 ///////////////////////// PICK DECK /////////////////////////
-
+function randomPickSettings() {
+  const settingsArray = [];
+  settingsArray.push(randomClass.splice(Math.floor(Math.random() * randomClass.length), 1)[0]);
+  settingsArray.push(randomType.splice(Math.floor(Math.random() * randomType.length), 1)[0]);
+  settingsArray.push(randomRarity.splice(Math.floor(Math.random() * randomRarity.length), 1)[0]);
+  if (settingsArray[0] === "neutral" && settingsArray[1] === "spell") {
+    let randomTypeIndex = randomType.indexOf("minion");
+    if (randomTypeIndex === -1) {
+      console.log("not enough class spells");
+      settingsArray[1] = "minion";
+    } else {
+      settingsArray[1] = randomType.splice(randomTypeIndex, 1, "spell")[0];
+    }
+  }
+  return settingsArray;
+}
 // User chooses a card
 function cardPick() {
+  if (custom) {
+    const pickDetails = randomPickSettings();
+    filteredPool = pools[pickDetails[0]][pickDetails[1]][pickDetails[2]];
+  }
   pickOptions = [];
-
   const selectedIndices = []; // To log indices and avoid duplicates
   let currentSelection = 0;
-
   // Build array of options for pick
   for (let i = 0; i < 3; i++) {
     do {
       currentSelection = Math.floor(Math.random() * filteredPool.length);
     } while (selectedIndices.includes(currentSelection) || maxxedAlready(filteredPool[currentSelection])); // Verify index has not already been selected and that two copies of card are not already in deck
     selectedIndices.push(currentSelection);
-
     // Add randomly selected card to pick
     pickOptions.push(filteredPool[currentSelection]);
   }
-
   renderPick(pickOptions);
   cardDisplay.addEventListener('click', cardPickHandler);
 }
-
 // Function that determines if given card is already a 2-of in the deck
 function maxxedAlready(proposedCard) {
   let numInDeck = 0;
@@ -127,40 +405,31 @@ function maxxedAlready(proposedCard) {
   }
   return false;
 }
-
-
 function cardPickHandler(e) {
   // Ensure target is a card image
   if (e.target && e.target.id.includes("img")) {
     // Remove event listener to avoid multiple triggers
     cardDisplay.removeEventListener('click', cardPickHandler);
-
     // Use id of target to determine which option was selected
     const position = +e.target.id.slice(-1);
-
     // Add choosen card to deck
     deck.push(pickOptions[position]);
-
-    updateProgress()
+    updateProgress();
     sortDeck();
     renderDeck(deck);
-
     // Restart card pick process until 30 picks are made
     if (deck.length < 30) {
       cardPick();
     }
-
     // If 30 picks have been made, advance the flow of the program
     if (deck.length === 30) {
       deckComplete();
     }
   }
-
   // If user clicks red x indicating they don't own the card
     if (e.target && e.target.id.includes("xbtn")) {
       // Removed unowned card and replace with new random card
       let newOption = filteredPool[Math.floor(Math.random() * filteredPool.length)];
-
       // TODO: Ensure that the newOption is not the same as one of the curent options
       while (maxxedAlready(newOption)) {
         newOption = filteredPool[Math.floor(Math.random() * filteredPool.length)];
@@ -169,8 +438,6 @@ function cardPickHandler(e) {
       renderPick(pickOptions);
     }
 }
-
-
 // Sort first by cost, and then alphabetically by card name
 function sortDeck() {
   deck.sort((cardA, cardB) => {
@@ -183,17 +450,14 @@ function sortDeck() {
     return cardA.name.localeCompare(cardB.name);
   });
 }
-
 function renderDeck(array) {
   cardName = document.getElementById('card-name');
   cardCost = document.getElementById('card-cost');
   cardName.innerHTML = '';
   cardCost.innerHTML = '';
-
   array.forEach(function(card) {
     counter = 0;
     // if (counter < 10) {
-
   // }
     cardName.innerHTML += `
       <div id="cards00" class="flex name-style">
@@ -201,7 +465,6 @@ function renderDeck(array) {
       </div>
     `
   })
-
   array.forEach(function(card) {
     cardCost.innerHTML += `
       <div class="flex cost-style">
@@ -211,7 +474,6 @@ function renderDeck(array) {
   })
   counter++
 }
-
 
 
 ///////////////////////// COMPLETE DECK /////////////////////////
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,93 +1,191 @@
+// localStorage.clear();
+// const masterPool = []; // All collectible cards
+// const heroes = []; // The nine original heroes to represent classes
+//
+// ///////////////////////// START DRAFT /////////////////////////
+//
+// document.addEventListener('DOMContentLoaded', () => {
+// axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1', {
+//   headers: {
+//     'X-Mashape-Key': 'gAeuReVzM3mshLLX97GlEfieDYDep1H5yDOjsn5z5VlqqZie5Q'
+//   }
+// }).then(response => {
+//
+//   // First nine cards of data are the heroes. Add them to heroes array
+//   for (let i = 0; i < 9; i++) {
+//     heroes.push(response.data.Basic[i]);
+//   }
+//
+//   // Iterate through all cards in data and push non-Hero cards to the master pool
+//   for (const set in response.data) {
+//     for (const card of response.data[set]) {
+//       if (card.type !== "Hero") {
+//         masterPool.push(card);
+//       }
+//     }
+//   }
+//
+//   localStorage.setItem("masterPool", JSON.stringify(masterPool));
+//   localStorage.setItem("heroes", JSON.stringify(heroes));
+//
+//   const draftBtn =  document.getElementById('draft-btn');
+//   draftBtn.classList.remove("inactive");
+//
+//   draftBtn.addEventListener('click', e => {
+//     let classCount = document.getElementById('classInput').value;
+//     let neutralCount = document.getElementById('neutralInput').value;
+//
+//     let legendaryCount = document.getElementById('legendInput').value;
+//     let epicCount = document.getElementById('epicInput').value;
+//     let rareCount = document.getElementById('rareInput').value;
+//
+//     let minionCount = document.getElementById('minionInput').value;
+//     let spellCount = document.getElementById('spellInput').value;
+//
+//     const setArray = [];
+//     let setOptions = document.getElementById('select').options;
+//     for (const set of setOptions) {
+//       if(set.selected) {
+//         setArray.push(set.value);
+//       }
+//     }
+//
+//     const formatRules = {
+//       'classCount': classCount,
+//       'neutralCount': neutralCount, // 30 - class
+//       'legendaryCount': legendaryCount,
+//       'epicCount': epicCount,
+//       'rareCount': rareCount,
+//       'minionCount': minionCount, //30 - spell
+//       'spellCount': spellCount,
+//       'setArray': setArray
+//     };
+//
+//
+//     localStorage.setItem("formatRules", JSON.stringify(formatRules));
+//
+//     console.log(localStorage.getItem("formatRules"));
+//
+//     //document.getElementById('neutralInput').value -= 1;
+//
+//
+//     window.location.href = "draft.html";
+//   });
+// });
+// });
+//
+// //Multi-Select Click Function
+// window.onmousedown = function (e) {
+//    var el = e.target;
+//    if (el.tagName.toLowerCase() == 'option' && el.parentNode.hasAttribute('multiple')) {
+//        e.preventDefault();
+//
+//        // toggle selection
+//        if (el.hasAttribute('selected')) el.removeAttribute('selected');
+//        else el.setAttribute('selected', '');
+//
+//        // hack to correct buggy behavior
+//        var select = el.parentNode.cloneNode(true);
+//        el.parentNode.parentNode.replaceChild(select, el.parentNode);
+//    }
+// };
+//
+
+
+///////////////////////////////////////////////////////////////////////////
+
 localStorage.clear();
 const masterPool = []; // All collectible cards
 const heroes = []; // The nine original heroes to represent classes
 
-///////////////////////// START DRAFT /////////////////////////
 
 document.addEventListener('DOMContentLoaded', () => {
-axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1', {
-  headers: {
-    'X-Mashape-Key': 'gAeuReVzM3mshLLX97GlEfieDYDep1H5yDOjsn5z5VlqqZie5Q'
-  }
-}).then(response => {
+ axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1', {
+   headers: {
+     'X-Mashape-Key': 'gAeuReVzM3mshLLX97GlEfieDYDep1H5yDOjsn5z5VlqqZie5Q'
+   }
+ }).then(response => {
 
-  // First nine cards of data are the heroes. Add them to heroes array
-  for (let i = 0; i < 9; i++) {
-    heroes.push(response.data.Basic[i]);
-  }
+   // First nine cards of data are the heroes. Add them to heroes array
+   for (let i = 0; i < 9; i++) {
+     heroes.push(response.data.Basic[i]);
+   }
 
-  // Iterate through all cards in data and push non-Hero cards to the master pool
-  for (const set in response.data) {
-    for (const card of response.data[set]) {
-      if (card.type !== "Hero") {
-        masterPool.push(card);
-      }
-    }
-  }
+   // Iterate through all cards in data and push non-Hero cards to the master pool
+   for (const set in response.data) {
+     for (const card of response.data[set]) {
+       if (card.type !== "Hero") {
+         masterPool.push(card);
+       }
+     }
+   }
 
-  localStorage.setItem("masterPool", JSON.stringify(masterPool));
-  localStorage.setItem("heroes", JSON.stringify(heroes));
+   localStorage.setItem("masterPool", JSON.stringify(masterPool));
+   localStorage.setItem("heroes", JSON.stringify(heroes));
 
-  const draftBtn =  document.getElementById('draft-btn');
-  draftBtn.classList.remove("inactive");
-
-  draftBtn.addEventListener('click', e => {
-    let classCount = document.getElementById('classInput').value;
-    let neutralCount = document.getElementById('neutralInput').value;
-
-    let legendaryCount = document.getElementById('legendInput').value;
-    let epicCount = document.getElementById('epicInput').value;
-    let rareCount = document.getElementById('rareInput').value;
-
-    let minionCount = document.getElementById('minionInput').value;
-    let spellCount = document.getElementById('spellInput').value;
-
-    const setArray = [];
-    let setOptions = document.getElementById('select').options;
-    for (const set of setOptions) {
-      if(set.selected) {
-        setArray.push(set.value);
-      }
-    }
-
-    const formatRules = {
-      'classCount': classCount,
-      'neutralCount': neutralCount, // 30 - class
-      'legendaryCount': legendaryCount,
-      'epicCount': epicCount,
-      'rareCount': rareCount,
-      'minionCount': minionCount, //30 - spell
-      'spellCount': spellCount,
-      'setArray': setArray
-    };
+   const draftBtn =  document.getElementById('draft-btn');
+   draftBtn.classList.remove("inactive");
 
 
-    localStorage.setItem("formatRules", JSON.stringify(formatRules));
+   draftBtn.addEventListener('click', e => {
 
-    console.log(localStorage.getItem("formatRules"));
+     const setArray = [];
+     let setOptions = document.getElementById('select').options;
+     for (const set of setOptions) {
+       if(set.selected) {
+         setArray.push(set.value);
+       }
+     }
+     localStorage.setItem("chosenSets", JSON.stringify(setArray));
 
-    //document.getElementById('neutralInput').value -= 1;
+
+     //get value of custom checkbox
+     let custom = true;
+     localStorage.setItem("custom", custom);
+     let customRules = {};
+
+     if (custom) {
+       let classCount = document.getElementById('classInput').value;
+
+       let legendaryCount = document.getElementById('legendInput').value;
+       let epicCount = document.getElementById('epicInput').value;
+       let rareCount = document.getElementById('rareInput').value;
+
+       let spellCount = document.getElementById('spellInput').value;
+
+       customRules = {
+         'classCount': classCount,
+         'legendaryCount': legendaryCount,
+         'epicCount': epicCount,
+         'rareCount': rareCount,
+         'spellCount': spellCount
+       };
+     }
+     localStorage.setItem("customRules", JSON.stringify(customRules));
 
 
-    window.location.href = "draft.html";
-  });
-});
+     // document.getElementById('neutralInput').value -= 1;
+
+
+     window.location.href = "draft.html";
+   });
+ });
 });
 
 //Multi-Select Click Function
 window.onmousedown = function (e) {
-   var el = e.target;
-   if (el.tagName.toLowerCase() == 'option' && el.parentNode.hasAttribute('multiple')) {
-       e.preventDefault();
+    var el = e.target;
+    if (el.tagName.toLowerCase() == 'option' && el.parentNode.hasAttribute('multiple')) {
+        e.preventDefault();
 
-       // toggle selection
-       if (el.hasAttribute('selected')) el.removeAttribute('selected');
-       else el.setAttribute('selected', '');
+        // toggle selection
+        if (el.hasAttribute('selected')) el.removeAttribute('selected');
+        else el.setAttribute('selected', '');
 
-       // hack to correct buggy behavior
-       var select = el.parentNode.cloneNode(true);
-       el.parentNode.parentNode.replaceChild(select, el.parentNode);
-   }
+        // hack to correct buggy behavior
+        var select = el.parentNode.cloneNode(true);
+        el.parentNode.parentNode.replaceChild(select, el.parentNode);
+    }
 };
 
 //Enable Sliders when checkbox selected
